23063841010014926af982890c5d8bb39056d56e
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-02-13 18:56:04 +0530

    Contact Form Done

--- client/src/components/ContactUs/ContactUs.jsx
+++ client/src/components/ContactUs/ContactUs.jsx
@@ -1,55 +1,38 @@
-import Faq from "../Faq/Faq";
-import classes from "./ContactUs.module.css";
+import React from 'react'
+import classes from "./ContactUs.module.css"
+import Button from "../common/Button/Button"
 
 const ContactUs = () => {
   return (
-    <section id="contact" className={classes.contact}>
-      <div className={classes.contactBox}>
-        <div className={classes.contentBox}>
-          <Faq />
-          {/* <div className={classes.persons}>
-
-            <div className={classes.personBox}>
-              <h3 className={classes.personHeading}>Shamsudheen</h3>
-              <div className={classes.details}>
-                <p>
-                  Chairman<br></br>Union KMCT
-                </p>
-                <a className={classes.anchor} href="tel:+917012824370">
-                  +91 7012 824 370
-                </a>
+    <>
+      <div className={classes.background}>
+            <div className={classes.registerForm}>
+              <h1 className={classes.heading}>Contact Us</h1>
+              <div className={classes.userData}>
+                <div className={classes.input_container}>
+                  <label className={classes.input_label}>
+                    Name<span className={classes.mandatory}>*</span>
+                  </label>
+                  <input className={classes.input}/>
+                </div>
+                <div className={classes.input_container}>
+                  <label className={classes.input_label}>
+                    Email<span className={classes.mandatory}>*</span>
+                  </label>
+                  <input className={classes.input}/>
+                </div>
+                <div className={classes.input_container}>
+                  <label className={classes.input_label}>
+                    Message<span className={classes.mandatory}>*</span>
+                  </label>
+                  <textarea className={classes.message}>Enter text Here</textarea>
+                </div>
               </div>
+              <div className={classes.button}><Button label="Send"/></div>
             </div>
-
-            <div className={classes.personBox}>
-              <h3 className={classes.personHeading}>Sithara Beegam</h3>
-              <div className={classes.details}>
-                <p>
-                  Vice Chairman<br></br>Union KMCT
-                </p>
-                <a className={classes.anchor} href="tel:+918592002134">
-                  +91 8592 002 134
-                </a>
-              </div>
-            </div>
-          </div> */}
-
-          <div className={classes.mapBox}>
-            <iframe
-              className={classes.map}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4809.059309848436!2d73.76092887672722!3d18.649282832139026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9f1ca8dab03%3A0x6237cfbd36f9acf9!2sD.Y.%20Patil%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1676150863219!5m2!1sen!2sin"
-              // width="600"
-              // height="450"
-              // style="border:0;"
-              // allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+    </>
+  )
+}
 
-export default ContactUs;
+export default ContactUs
