7966563f13c99d082404db815a1b53254cad3e2f
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-02-13 18:33:11 +0530

    single event page redesigned

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import About from "../components/About/About";
+import EventSection from "../components/EventSection/EventSection";
+import classes from "./AboutPage.module.css";
 
 const AboutPage = () => {
   return (
     <>
-      <div>asfbadkfjbdaojfb</div>
+      <div className={classes.about_page}>
+        <About />
+        <EventSection />
+      </div>
     </>
   );
 };
