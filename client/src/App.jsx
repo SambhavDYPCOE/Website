3d49d4b55a3d1e823548ab6105fabe2505bb6f76
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-02-14 17:49:19 +0530

    grabit pop up added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import AboutPage from "./pages/AboutPage";
 import Register from "./components/common/Register/Register";
 import ContactUs from "./components/ContactUs/ContactUs";
 import Loading from "./components/common/Loading/Loading";
+import Popup from "./components/common/Popup/Popup";
 // import SwupOverlayTheme from "@swup/overlay-theme";
 // import Swup from "swup";
 
@@ -51,6 +52,7 @@ const App = () => {
           height: "auto",
         }}
       > */}
+          <Popup />
           <Navbar />
           <Routes>
             <Route path="/" element={<Homepage />} />
---
+++ client/src/components/common/Popup/Popup.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import classes from "./Popup.module.css";
+import whatsapp from "../../../assets/whatsapp.svg";
+import cross from "../../../assets/cross.svg";
+import GrabBitBtn from "../GrabBitBtn/GrabBitBtn";
+import grabbits from "../../../assets/grabbits.png";
+
+const Popup = () => {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    const popupShown = localStorage.getItem("grabBitPopUp");
+    if (!popupShown) {
+      setTimeout(() => {
+        setShowPopup(true);
+        localStorage.setItem("grabBitPopUp", true);
+      }, 7000);
+    }
+  }, []);
+
+  function handleClose() {
+    setShowPopup(false);
+  }
+
+  return (
+    <>
+      {showPopup && (
+        <>
+          <div className={classes.blur}>
+            <div className={classes.card}>
+              <img
+                className={classes.close}
+                onClick={() => {
+                  handleClose();
+                }}
+                src={cross}
+                alt="close"
+              />
+              <img
+                className={classes.grabbit_image}
+                src={grabbits}
+                alt="grabbits"
+              />
+              <h2 className={classes.text}>Join Our Whatsapp Group</h2>
+              <h1 className={classes.subtext}>
+                Stay updated with all the opportunites.
+              </h1>
+              <div className={classes.btn_container}>
+                <a
+                  href="https://chat.whatsapp.com/KBxP1M7GT7mCh4PORsfN0H"
+                  target="blank"
+                >
+                  <GrabBitBtn bgColor="#25D366">
+                    <img
+                      className={classes.btn_svg}
+                      src={whatsapp}
+                      alt="whatsapp"
+                    />
+                    <span className={classes.btn_text}>Whatsapp</span>
+                  </GrabBitBtn>
+                </a>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Popup;
